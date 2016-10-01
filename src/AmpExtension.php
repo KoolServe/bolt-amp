14b989ed528e623e68e51da194deabf113cdef7e
Chris Hilsdon <chris@koolserve.uk>
2016-10-01 18:48:21 +0100

    Initial commit :rocket:

---
+++ src/AmpExtension.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bolt\Extension\Koolserve\Amp;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * Amp extension class.
+ *
+ * @author Chris Hilsdon <chris@koolserve.uk>
+ */
+class AmpExtension extends SimpleExtension
+{
+
+}
